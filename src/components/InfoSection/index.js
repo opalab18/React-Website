3bc0cc02c927a8e85382d8537446e0258ae9bd3b
Opala <opalic74@gmail.com>
2021-10-17 21:16:09 +0200

    Created Infosection

---
+++ src/components/InfoSection/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Button } from '../ButtonElement'
+import { 
+    InfoContainer, 
+    InfoWrapper, 
+    InfoRow, 
+    Column1, 
+    Column2, 
+    TextWrapper, 
+    TopLine, 
+    Heading, 
+    Subtitle, 
+    BtnWrap,
+    ImgWrap,
+    Img } from './InfoElements'
+
+
+const InfoSection = () => {
+    return (
+        <>
+            <InfoContainer>
+                <InfoWrapper>
+                    <InfoRow>
+                        <Column1>
+                            <TextWrapper>
+                                <TopLine>Top</TopLine>
+                                <Heading>Heading</Heading>
+                                <Subtitle>Subtitle</Subtitle>
+                                <BtnWrap>
+                                    <Button to="home">Button</Button>
+                                </BtnWrap>
+                            </TextWrapper>
+                        </Column1>
+                        <Column2>
+                            <ImgWrap>
+                                <Img />
+                            </ImgWrap>
+                        </Column2>
+                    </InfoRow>
+                </InfoWrapper>
+            </InfoContainer>
+        </>
+    )
+}
+
+export default InfoSection
